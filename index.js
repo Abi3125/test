061f2e051128ad42a407900ca537c34f212f51c9
Abi3125 <abyssgate0930@gmail.com>
2024-06-09 01:48:05 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function getDice1(){
+    var dice1 = Math.floor(Math.random()*153);
+    var dice2 = Math.floor(Math.random()*3);
+    var dice3 = Math.floor(Math.random()*2);
+    document.getElementById('condice1').innerHTML = (dice1+1);
+    document.getElementById('condice2').innerHTML = (dice2+1);
+    document.getElementById('condice3').innerHTML = (dice3+1);
+}
