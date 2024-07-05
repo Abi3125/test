6cc3de74655881890db293dc8efbc2373089fbc2
Abi3125 <abyssgate0930@gmail.com>
2024-07-06 01:34:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getDice1(){
-    var dice1 = Math.floor(Math.random()*153);
+    var dice1 = Math.floor(Math.random()*155);
     var dice2 = Math.floor(Math.random()*3);
     var dice3 = Math.floor(Math.random()*2);
     document.getElementById('condice1').innerHTML = (dice1+1);
